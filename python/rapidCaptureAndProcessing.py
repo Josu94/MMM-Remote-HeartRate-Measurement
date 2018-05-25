e5ab3ef0f1e3692d2a40996bd4ab0fd65bc80e62
Josua Öhler <jo026@hdm-stuttgart.de>
2018-05-25 21:22:57 +0200

    Add new Python file rapidCaptureAndProcessing.py

---
+++ python/rapidCaptureAndProcessing.py
@@ -0,0 +1,23 @@
+import time
+import picamera
+
+frames = 60
+
+def filenames():
+    frame = 0
+    while frame < frames:
+        yield 'image%02d.jpg' % frame
+        frame += 1
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.framerate = 30
+    camera.start_preview()
+    # Give the camera some warm-up time
+    time.sleep(2)
+    start = time.time()
+    camera.capture_sequence(filenames(), use_video_port=True)
+    finish = time.time()
+print('Captured %d frames at %.2ffps' % (
+    frames,
+    frames / (finish - start)))
